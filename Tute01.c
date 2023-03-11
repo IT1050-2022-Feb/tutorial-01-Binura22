44da2f060e322267d7bd93c0427edea9f547320a
it22601674 <127595699+Binura22@users.noreply.github.com>
2023-03-11 16:04:45 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,26 @@
 #include <stdio.h>
 
 int main() {
-  
+  // variable declaration
+  float std1,std2; 
+  float sum,average;
+   
+   //get two marks by user
+   printf( "Input student 1 marks:" );
+   scanf( "%f", &std1 );
+   
+   printf( "Input student 2 marks:" );
+   scanf( "%f", &std2);
+   
+   //add two marks
+   sum = std1 + std2;
+   
+   //find average
+   average = sum / 2;
+   
+   //display average
+   printf( "Average of the two marks is:%.2f", average );
+   
   return 0;
 }
 
